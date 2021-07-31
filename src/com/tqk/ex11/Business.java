27a8c17e6ba06f55f16270c01390ca13ec000ed8
tianqikai <953811508@qq.com>
2021-07-31 09:03:43 +0800

    add

---
+++ src/com/tqk/ex11/Business.java
@@ -0,0 +1,22 @@
+package com.tqk.ex11;
+
+public class Business  {
+
+ 
+
+ public boolean doSomeThing() {
+
+        System.out.println("执行业务逻辑");
+
+        return true;
+
+    }
+ public void doSomeThing2() {
+
+        String s = "执行业务逻辑2";
+
+        System.out.println(s);
+
+    }
+
+}
---
+++ src/com/tqk/ex11/Test.java
@@ -0,0 +1,13 @@
+package com.tqk.ex11;
+
+public class Test {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        new Business().doSomeThing();   
+        new Business().doSomeThing2();   
+
+    }
+}
